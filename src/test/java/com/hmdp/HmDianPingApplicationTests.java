4babade07a50aded80a660048062b28177214989
cszw <212050275@hdu.edu.cn>
2023-06-17 12:17:45 +0800

    基于Redis实现共享session登录

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,22 @@
 package com.hmdp;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
 
+    @Test
+    public void testRedis(){
+        stringRedisTemplate.opsForValue().set("ggg", "gggggg", 2, TimeUnit.SECONDS);
+        String d = stringRedisTemplate.opsForValue().get("ggg");
+        System.out.println(d);
+    }
 
 }
