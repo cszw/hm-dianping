ee7c79d7df31e4a38046546227b4cd06346025ea
cszw <212050275@hdu.edu.cn>
2023-06-17 17:25:52 +0800

    商户查询缓存

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,5 +1,9 @@
 package com.hmdp;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Shop;
+import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -7,16 +11,22 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Resource
-    StringRedisTemplate stringRedisTemplate;
+    private CacheClient cacheClient;
+
+    @Resource
+    private ShopServiceImpl shopService;
+
 
     @Test
-    public void testRedis(){
-        stringRedisTemplate.opsForValue().set("ggg", "gggggg", 2, TimeUnit.SECONDS);
-        String d = stringRedisTemplate.opsForValue().get("ggg");
-        System.out.println(d);
+    public void testSaveShop() throws InterruptedException {
+        Shop shop = shopService.getById(1L);
+
+        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }
 
 }
